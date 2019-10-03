eb8c80650cadeff6439cc2217036879ceab250ff
Bryce-7034 <kellyb@wlhs.wlwv.k12.or.us>
2019-10-03 09:54:31 -0700

    added the three from the worksheet

--- Drinkage.java
+++ Drinkage.java
@@ -1,4 +1,13 @@
 import java.util.Scanner;
 public class Drinkage{
-
+  public static void main(String[]args){
+    Scanner scan = new Scanner(System.in);
+    System.out.println("please enter your age");
+    int age = scan.nextInt();
+    if(age >= 21){
+      System.out.println("you are old enough to drink");
+    }else{
+      System.out.println("you are not old enough to drink");
+    }
+  }
 }
