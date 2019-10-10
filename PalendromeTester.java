277843d85dd173454ee47cabfede8bf27e1c164b
Bryce-7034 <kellyb@wlhs.wlwv.k12.or.us>
2019-10-10 09:55:11 -0700

    added PalendromeTester

---
+++ PalendromeTester.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class PalendromeTester{
+  public static void main(String[]args){
+    Scanner scan = new Scanner(System.in);
+    boolean palendrome = true;
+    String y = "y";
+    String n ="n";
+    while (palendrome){
+      System.out.println("would you like to enter a palendrome, (y/n)");
+      String enter = scan.next();
+      if (enter.equals(y)){
+        
+      }
+
+    }
+  }
+}
