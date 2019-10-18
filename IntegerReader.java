23c83e55292017c0ef8e4e6b2529db2edb786a26
Bryce-7034 <kellyb@wlhs.wlwv.k12.or.us>
2019-10-18 09:40:04 -0700

    added rockpaperscissor

---
+++ IntegerReader.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class IntegerReader{
+  public static void main(String[]args){
+    Scanner scan = new Scanner(System.in);
+     System.out.println("please enter an integer");
+     String input = scan.nextLine();
+     
+  }
+}
--- Multiples.java
+++ Multiples.java
@@ -7,18 +7,21 @@ public class Multiples{
     final int MAX_ROWS = 5;
     Scanner scan = new Scanner(System.in);
     do {
-      if (max < multiple) System.out.print("Max must be greater than"
-      +" or equal to multiple\n");
-      if (max < 0) System.out.println("Max cannot be less than 0");
-      if (multiple < 0) System.out.println("Multiple cannot be less than 0");
       System.out.println("Please input the max output number");
       if (scan.hasNextInt()) max = scan.nextInt();
+      else continue;
       System.out.println("Please input the integer you want the multiples of");
       if (scan.hasNextInt()) multiple = scan.nextInt();
-    } while (max < multiple || max < 0 || multiple < 0);
+      else continue;
+      if (max < multiple) System.out.print("Max must be greater than"
+      +" or equal to multiple\n");
+      if (max <= 0) System.out.println("Max cannot be less than or equal to 0");
+      if (multiple < 0) System.out.println("Multiple cannot be less than 0");
+      if (multiple == 0) System.out.println("A multiple of 0 is 0");
+    } while (max < multiple || max <= 0 || multiple <= 0);
     for (int i = 0; max >= (multiple * i); i++){
-      for (int rows = 1; rows <= MAX_ROWS; rows++){
-        System.out.print(multiple * i +" ");
+      for (int rows = 1; rows <= MAX_ROWS && max >= multiple*i; rows++){
+        System.out.print((multiple * i) +" ");
         i++;
       }
       System.out.print("\n");
