26cb5cc4a08152971d0f98fd584816883b6e3db6
Bryce-7034 <kellyb@wlhs.wlwv.k12.or.us>
2019-10-22 09:46:57 -0700

    updated multiple things

--- IntegerReader.java
+++ IntegerReader.java
@@ -2,9 +2,27 @@ import java.util.Scanner;
 
 public class IntegerReader{
   public static void main(String[]args){
+    int even = 0;
+    int odd = 0;
+    int zeros = 0;
+    boolean run = true;
     Scanner scan = new Scanner(System.in);
-     System.out.println("please enter an integer");
-     String input = scan.nextLine();
-     
+    System.out.println("please enter an integer");
+    String input = scan.nextLine();
+      for(int i = 0; i <= input.length() - 1; i++){
+        String num = "-" + input.charAt(i);
+        if (num.equalsIgnoreCase("-2") || num.equalsIgnoreCase("-4")
+        || num.equalsIgnoreCase("-6") || num.equalsIgnoreCase("-8")){
+          even++;
+        } else if (num.equalsIgnoreCase("-3") || num.equalsIgnoreCase("-5")
+        || num.equalsIgnoreCase("-7") || num.equalsIgnoreCase("-9") || num.equalsIgnoreCase("-1")){
+          odd++;
+        }else if (num.equalsIgnoreCase("-0")){
+          zeros++;
+      }
+    }
+    System.out.println("number of zeros: "+ zeros);
+    System.out.println("number of evens: "+ even);
+    System.out.println("number of odds: "+ odd);
   }
 }
--- Test.java
+++ Test.java
@@ -3,7 +3,7 @@ public class Test{
     final int MAX = 10;
     final int total = 10;
     final int sum = 8;
-    "I don't know what you expected"
+    System.out.println("I don't know what you expected:");
     for (int val = 200; val >= 0; val -= 1)
          if( val % 4 != 0)
               System.out.println(val);
